cbeb7aecfdc89d6969216bb31c4c855b458d4a55
unknown <suporte@csamg.com.br>
2025-02-03 12:53:57 -0300

    Removi o Base (VITE)

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,7 @@ function App() {
         return (
           <div className="w-full max-w-md px-6">
             <div className="mb-12">
-              <img
-                src="/DineeLogin/public/logo.png"
-                alt="DINEE"
-                className="h-12 mx-auto"
-              />
+              <img src="public/logo.png" alt="DINEE" className="h-12 mx-auto" />
             </div>
 
             <div className="space-y-4">
@@ -55,17 +51,13 @@ function App() {
 
             <div className="mt-8 space-y-4">
               <button className="w-full bg-white flex items-center justify-center gap-3 py-3 border border-gray-300 rounded-3xl hover:bg-gray-50 transition-colors">
-                <img
-                  src="/DineeLogin/public/google.png"
-                  alt="Google"
-                  className="w-5 h-5"
-                />
+                <img src="public/google.png" alt="Google" className="w-5 h-5" />
                 <span className="text-gray-700">Continue com Google</span>
               </button>
 
               <button className="w-full bg-white flex items-center justify-center gap-3 py-3 border border-gray-300 rounded-3xl hover:bg-gray-50 transition-colors">
                 <img
-                  src="/DineeLogin/public/logotipo-da-apple.png"
+                  src="public/logotipo-da-apple.png"
                   alt="Apple"
                   className="w-5 h-5"
                 />
@@ -80,7 +72,7 @@ function App() {
           <div className="w-full max-w-md px-6 text-center">
             <div className="mb-12">
               <img
-                src="/DineeLogin/public/logo.png"
+                src="/public/logo.png"
                 alt="DINEE"
                 className="h-12 mx-auto"
               />
